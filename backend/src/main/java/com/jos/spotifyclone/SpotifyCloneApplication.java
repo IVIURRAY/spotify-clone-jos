aeb5757bc9ef7b5e6e828a48010486b168ca2a62
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-10-29 14:02:08 +0000

    project setup

---
+++ backend/src/main/java/com/jos/spotifyclone/SpotifyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.jos.spotifyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jos/spotifyclone/SpotifyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jos.spotifyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
