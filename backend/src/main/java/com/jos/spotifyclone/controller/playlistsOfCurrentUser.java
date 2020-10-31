6b03c0e3cbe68efd3acf7178b670303b15931ae3
Iulia Danilov <tmdoppelganger@gmail.com>
2020-10-31 17:53:36 +0200

    /user/playlists added

---
+++ backend/src/main/java/com/jos/spotifyclone/controller/playlistsOfCurrentUser.java
@@ -0,0 +1,29 @@
+package com.jos.spotifyclone.controller;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.jos.spotifyclone.services.SpotifyConnect;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequestMapping("/user/playlists")
+@RestController
+public class playlistsOfCurrentUser {
+
+    @Autowired
+    SpotifyConnect spotifyConnect;
+
+    @GetMapping
+    public @ResponseBody Paging<PlaylistSimplified> playlistsOfCurrentUser() throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().getListOfCurrentUsersPlaylists().build().execute();
+    }
+}
