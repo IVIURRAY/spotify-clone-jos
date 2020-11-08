35eb867b53f34e68e6618782406a7bc134814529
Almir James Lucena <lucenaalmirjames123@gmail.com>
2020-11-08 14:27:21 +0800

    added cors

--- backend/src/main/java/com/jos/spotifyclone/config/WebConfig.java
+++ backend/src/main/java/com/jos/spotifyclone/config/WebConfig.java
@@ -1,5 +1,21 @@
 package com.jos.spotifyclone.config;
 
-public class WebConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("http://localhost:3000/**");
+		
+	}
+
+	
+	
+	
 }
