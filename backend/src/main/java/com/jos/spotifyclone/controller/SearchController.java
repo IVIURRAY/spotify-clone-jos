64f3d048fe9e34276e6b6877de0cfa17ac20c0e5
Iulia Danilov <tmdoppelganger@gmail.com>
2020-11-08 12:15:13 +0200

    Update SearchController.java
    fixed

--- backend/src/main/java/com/jos/spotifyclone/controller/SearchController.java
+++ backend/src/main/java/com/jos/spotifyclone/controller/SearchController.java
@@ -5,12 +5,11 @@ import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.IModelObject;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-
+@RequestMapping("api/search")
+@RestController
 public class SearchController {
     @Autowired
     SpotifyConnect spotifyConnect;
