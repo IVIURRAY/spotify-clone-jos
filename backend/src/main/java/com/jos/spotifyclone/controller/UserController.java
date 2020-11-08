acaf3990c2fe3c1f53ec0c71efe5c3a0757d622e
Almir James Lucena <lucenaalmirjames123@gmail.com>
2020-11-08 15:05:08 +0800

    user controller refactored

--- backend/src/main/java/com/jos/spotifyclone/controller/UserController.java
+++ backend/src/main/java/com/jos/spotifyclone/controller/UserController.java
@@ -3,11 +3,15 @@ package com.jos.spotifyclone.controller;
 
 import com.jos.spotifyclone.services.SpotifyConnect;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.PagingCursorbased;
+import com.wrapper.spotify.model_objects.specification.PlayHistory;
+import com.wrapper.spotify.model_objects.specification.Recommendations;
 import com.wrapper.spotify.model_objects.specification.User;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,20 @@ public class UserController {
     User currentUserProfile() throws ParseException, SpotifyWebApiException, IOException {
         return spotifyConnect.getSpotifyApi().getCurrentUsersProfile().build().execute();
     }
+    
+    @GetMapping("/recentTracks/")
+    public @ResponseBody
+    PagingCursorbased<PlayHistory> getRecentTracks() throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().getCurrentUsersRecentlyPlayedTracks().build().execute();
+    }
+    
+    @GetMapping("/recommended/")
+    public @ResponseBody
+    Recommendations getRecommended(@RequestParam String seed) throws ParseException, SpotifyWebApiException, IOException {
+        var availableGenreSeeds = spotifyConnect.getSpotifyApi().getAvailableGenreSeeds();
+        return spotifyConnect.getSpotifyApi().getRecommendations().seed_genres(seed).build().execute();
+    }
+
 }
 
 
