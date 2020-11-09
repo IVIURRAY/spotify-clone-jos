b7510f2c4285e2337e82da45f9ffb54edebc64ae
Iulia Danilov <tmdoppelganger@gmail.com>
2020-11-09 16:52:20 +0200

    updated wrapper version,new search endpoints
    followed artists,saved albums/shows/tracks,remove albums/shows/tracks
    added examples too

--- backend/src/main/java/com/jos/spotifyclone/controller/UserController.java
+++ backend/src/main/java/com/jos/spotifyclone/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jos.spotifyclone.controller;
 
 
 import com.jos.spotifyclone.services.SpotifyConnect;
+import com.wrapper.spotify.enums.ModelObjectType;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.specification.*;
 import org.apache.hc.core5.http.ParseException;
@@ -42,6 +43,50 @@ public class UserController {
     @GetMapping("/playlist/")
     public @ResponseBody Paging<PlaylistSimplified> playlistsOfCurrentUser() throws ParseException, SpotifyWebApiException, IOException {
         return spotifyConnect.getSpotifyApi().getListOfCurrentUsersPlaylists().build().execute();
+    }
+
+    @GetMapping("/followedArtists/")
+    public @ResponseBody PagingCursorbased<Artist> followedArtists() throws ParseException, SpotifyWebApiException, IOException {
+        final ModelObjectType type = ModelObjectType.ARTIST;
+        return spotifyConnect.getSpotifyApi().getUsersFollowedArtists(type).build().execute();
+    }
+
+    @GetMapping("/savedAlbums/")
+    public @ResponseBody Paging<SavedAlbum> savedAlbums() throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().getCurrentUsersSavedAlbums().build().execute();
+    }
+
+    @GetMapping("/savedShows/")
+    public @ResponseBody Paging<SavedShow> savedShows() throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().getUsersSavedShows().build().execute();
+    }
+
+    @GetMapping("/savedTracks/")
+    public @ResponseBody Paging<SavedTrack> savedTracks() throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().getUsersSavedTracks().build().execute();
+    }
+
+    //run http://localhost:8080/api/user/savedAlbums/ first to find id's
+    //http://localhost:8080/api/user/removeAlbums/?ids=<replace with albums id>
+    @GetMapping("/removeAlbums/")
+    public @ResponseBody String removeAlbums(@RequestParam String ids) throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().removeAlbumsForCurrentUser(ids).build().execute();
+    }
+
+    //run http://localhost:8080/api/user/savedShows/ first to find id's
+    //http://localhost:8080/api/user/removeShows/?ids=<replace with shows id>
+    @GetMapping("/removeShows/")
+    public @ResponseBody
+    String removeShows(@RequestParam String ids) throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().removeUsersSavedShows(ids).build().execute();
+    }
+
+    //run http://localhost:8080/api/user/savedTracks/ first to find id's
+    //http://localhost:8080/api/user/removeTracks/?ids=<replace with tracks id>
+    @GetMapping("/removeTracks/")
+    public @ResponseBody
+    String removeTracks(@RequestParam String ids) throws ParseException, SpotifyWebApiException, IOException {
+        return spotifyConnect.getSpotifyApi().removeUsersSavedTracks(ids).build().execute();
     }
 }
 
