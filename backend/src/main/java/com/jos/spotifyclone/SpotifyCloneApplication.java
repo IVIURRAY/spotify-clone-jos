e46a0646cf92326c912cdea46680f716e77e362d
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-11-10 21:25:37 +0000

    Adding frontend url as app.prop

--- backend/src/main/java/com/jos/spotifyclone/SpotifyCloneApplication.java
+++ backend/src/main/java/com/jos/spotifyclone/SpotifyCloneApplication.java
@@ -1,5 +1,6 @@
 package com.jos.spotifyclone;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpotifyCloneApplication {
+
+	@Value("spotify.frontend.url")
+	String FRONTEND_URL;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyCloneApplication.class, args);
@@ -18,7 +22,7 @@ public class SpotifyCloneApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/*").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/api/*").allowedOrigins(FRONTEND_URL);
 			}
 		};
 	}
