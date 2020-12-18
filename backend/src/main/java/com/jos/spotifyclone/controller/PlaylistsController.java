14c210860671a769d3deb4a5f383e48459926e3c
Iulia Danilov <tmdoppelganger@gmail.com>
2020-12-18 23:13:23 +0200

    cron for tokens

--- backend/src/main/java/com/jos/spotifyclone/controller/PlaylistsController.java
+++ backend/src/main/java/com/jos/spotifyclone/controller/PlaylistsController.java
@@ -17,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/playlists")
--- backend/src/main/java/com/jos/spotifyclone/controller/SpotifyAuthController.java
+++ backend/src/main/java/com/jos/spotifyclone/controller/SpotifyAuthController.java
@@ -4,7 +4,10 @@ import com.jos.spotifyclone.services.SpotifyConnect;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
